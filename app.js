fc2c6f69cfefd90c104971995f3b7ef1e5c3dbf6
salman1921 <hafizsalmanyousuf@gmail.com>
2023-09-12 21:21:35 +0500

    add two files

---
+++ app.js
@@ -0,0 +1,134 @@
+// var arr = ["salman ", "Yousuf " , 12 , true]
+
+
+
+// end se remove krega
+// arr.pop();
+
+// end me add krdega
+// arr.push("ZS");
+
+// start se remove krega
+// arr.shift(); 
+
+// kahi se bh add krdega or khi se bh remove krdega
+// arr.splice(0,1,"sallu")
+
+// start se add krega
+// arr.unshift("adil" , 130)
+
+// console.log(arr);
+
+// create an empty array and insert the value through user input with index number...
+
+// var create = [];
+
+// create[0]="Salman";
+// create[1]="Yousuf";
+// create[2]="ZS";
+// create[3]="father";
+// create    [4]="mother";
+
+// console.log(create[3]);
+
+
+// 1. Declare an empty array using JS literal notation to store
+// student names in future.
+
+// var students =[];
+
+// 2. Declare an empty array using JS object notation to store
+// student names in future.
+
+// var students2 =[];
+
+
+
+// 3. Declare and initialize a strings array.
+
+// var students =["salman"];
+
+// 4. Declare and initialize a numbers array.
+// var students =[12];
+
+
+// 5. Declare and initialize a boolean array.
+// var students =[true];
+
+
+// 6. Declare and initialize a mixed array.
+// var students =["salman",12,true];
+
+
+// 7. Declare and Initialize an array and store available
+// education qualifications in Pakistan (e.g. SSC, HSC, BCS,
+// BS, BCOM, MS, M. Phil., PhD). Show the listed
+// qualifications in your browser like:
+
+
+// var educationqualification = ["Bs","Bcom","MS","Phil","Phd","HSC","BCS","BS","SSC",];
+
+// console.log("Qualification", educationqualification);
+
+// 8. Write a program to store 3 student names in an array.Take
+// another array to store score of these three students.
+// Assume that total marks are 500 for each student, display
+// the scores & percentages of students like:
+
+// Create an array to store student names
+
+// var studentNames = ["John", "Alice", "Bob"];
+
+// Create an array to store student scores
+// var studentScores = [420, 350, 480];
+
+// Calculate the total marks (assumed to be 500 for each student)
+// var totalMarks = 800;
+
+// Calculate and display the scores and percentages of students
+// for (var i = 0; i < studentNames.length; i++) {
+//   var name = studentNames[i];
+//   var score = studentScores[i];
+  
+//   // Calculate the percentage
+//   var percentage = (score / totalMarks) * 100;
+
+//   // Display the results
+//   console.log(name + "'s Score: " + score);
+//   console.log(name + "'s Percentage: " + percentage.toFixed(2) + "%");
+//   console.log("-------------------------");
+
+// }
+// var perc = studentScores[0]/totalMarks*100;
+// var perc1 = studentScores[1]/totalMarks*100;
+// var perc2= studentScores[2]/totalMarks*100;
+
+
+// console.log("Score of John is 420","Percentage",perc)
+// console.log("Score of Alice is 350","Percentage",perc1)
+// console.log("Score of Bob is 480","Percentage",perc2)
+
+
+// 10. Write a program to store student scores in an array &
+// sort the array in ascending order using Array’s sort
+// method.
+
+
+// var scores = [320 , 330,340,350];
+// Create an array to store student scores
+var studentScores = [85, 70, 92, 65, 78, 88];
+
+// Sort the array in ascending order
+studentScores.sort();
+
+// Display the sorted array
+console.log(studentScores);
+
+
+
+
+
+
+
+
+
